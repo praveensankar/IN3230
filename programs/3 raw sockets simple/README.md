f70325dab4c574542a7f5a0b817a446ab51756e7
Praveen <praveensankar000@gmail.com>
2021-09-09 00:57:11 +0200

    week 2 oracle program - raw socket

--- programs/3 raw sockets simple/README.md
+++ programs/3 raw sockets simple/README.md
@@ -1,10 +1,3 @@
-In this session, we're going to implement client and server applications
-that use RAW sockets to communicate to each other in a chat session.
-
-topo_p2p.py is the python script that generates the mininet topology.
-
-select/poll will be used to monitor the activity of stdin and raw socket descriptor
-
 
 Usage:
 
@@ -14,6 +7,3 @@ Usage:
   (You should have used -Y argument in the ssh command: ssh -Y debian@ip_address_of_your_VM
 - From the xterm consoles, run ./receiver at node h2 and ./sender at node h1
 
-  Practise and implement new features in the applications.
-
-  Good luck :-)
---
+++ programs/3 raw sockets simple/ifaddrs_test.c
@@ -0,0 +1,55 @@
+//
+// Created by praveen on 9/8/21.
+//
+
+#include <ifaddrs.h>		/* getifaddrs */
+#include <stdio.h>
+#include <sys/socket.h>
+#include <linux/if_packet.h>
+
+void display_mac(struct ifaddrs *ifp);
+
+void print_mac_from_interface()
+{
+    struct ifaddrs *ifaces, *ifp;
+    /* Enumerate interfaces: */
+
+    getifaddrs(&ifaces);
+
+    /* Walk the list looking for ifaces interesting to us */
+    //printf("Interface list:\n");
+    for (ifp = ifaces; ifp != NULL; ifp = ifp->ifa_next) {
+        /* We make certain that the ifa_addr member is actually set: */
+        if (ifp->ifa_addr != NULL &&
+            ifp->ifa_addr->sa_family == AF_PACKET) {
+            display_mac(ifp);
+            printf("\n");
+        }
+    }
+
+
+    /* Free the interface list */
+    freeifaddrs(ifaces);
+    return;
+}
+
+// run the ifconfig command in the terminal to verify the information
+void display_mac(struct ifaddrs *ifp)
+{
+    struct sockaddr_ll *sock_addr = (struct sockaddr_ll *) ifp->ifa_addr;
+    printf("\n -----------new mac address-------------");
+    printf("\n interface name : %s", ifp->ifa_name);
+    printf("\n Interface address : ");
+    printf("\n \t  family : %d", sock_addr->sll_family);
+    printf("\n \t interface index : %d ", sock_addr->sll_ifindex);
+   // printf("\n \t physical address : %d", sock_addr->sll_addr);
+    printf("\n \t packet type : %d ", sock_addr->sll_pkttype);
+}
+
+
+
+int main()
+{
+    print_mac_from_interface();
+    return 0;
+}
